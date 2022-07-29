7f4d097360c14f60341ec8667679b48cac52589c
Parham Abolghasemi <parhamab17@gmail.com>
2022-07-29 20:52:37 +0430

    bugs fixed

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,39 +1,43 @@
 import React from "react";
-// react router dom
-import { Link } from "react-router-dom";
-// mui components
+import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
 // components
-import Container from "@mui/material/Container";
 import Blogs from "../blogs/Blogs";
+import Authors from "../authors/Authors";
 
 const HomePage = () => {
   return (
     <Container maxWidth="lg">
       <Grid container spacing={2} padding={3}>
         <Grid item xs={12} md={3} mt={4}>
-          <Typography
-            variant="h5"
-            component={"h3"}
-            mb={3}
-            fontWeight={700}
-            color="text.secondary"
-          >
-            Authors
-          </Typography>
+          <Link to={`/authors`}>
+            <Typography
+              variant="h5"
+              component={"h3"}
+              mb={3}
+              fontWeight={700}
+              color="text.secondary"
+            >
+              Authors
+            </Typography>
+          </Link>
+          <Authors />
         </Grid>
 
         <Grid item xs={12} md={9} mt={4}>
-          <Typography
-            variant="h5"
-            component={"h3"}
-            mb={3}
-            fontWeight={700}
-            color="text.secondary"
-          >
-            Blogs
-          </Typography>
+          <Link to={`/blogs`}>
+            <Typography
+              variant="h5"
+              component={"h3"}
+              mb={3}
+              fontWeight={700}
+              color="text.secondary"
+            >
+              Blogs
+            </Typography>
+          </Link>
           <Blogs />
         </Grid>
       </Grid>
