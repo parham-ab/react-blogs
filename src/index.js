1f730fdb2e182ead5d71a742933f4edc8cb3af14
Parham Abolghasemi <parhamab17@gmail.com>
2022-07-29 18:06:37 +0430

    set up graphql

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// apollo & graphql imports
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 // react-router-dom
 import { BrowserRouter } from "react-router-dom";
 // styles
 import "./assets/styles/index.scss";
 
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API,
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
 );
