3040f61897acf896b217307de55b900e6ea55a0f
Parham Abolghasemi <parhamab17@gmail.com>
2022-07-29 20:48:54 +0430

    routes declared

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import { Routes, Route, Navigate } from "react-router-dom";
 // components
 import LayOut from "./components/layout";
 import HomePage from "./components/home/HomePage";
+import BlogsPage from "./components/blogs/BlogsPage";
+import AuthorPage from "./components/authors/AuthorPage";
+import Blog from "./components/blogs/Blog";
+import Author from "./components/authors/Author";
 
 const App = () => {
   return (
     <LayOut>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/blogs" element={<Blog />} />
+        <Route path="/blogs/:slug" element={<BlogsPage />} />
+        <Route path="/authors" element={<Author />} />
+        <Route path="/authors/:slug" element={<AuthorPage />} />
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
     </LayOut>
