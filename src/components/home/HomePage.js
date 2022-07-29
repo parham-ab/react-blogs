c39c2d43b95e70aea261e5c4d3ca92975f27780f
Parham Abolghasemi <parhamab17@gmail.com>
2022-07-29 17:58:41 +0430

    home page content updated

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+// react router dom
+import { Link } from "react-router-dom";
+// mui components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
 
 const HomePage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Container maxWidth="lg">
+      <Grid container spacing={2} padding={3}>
+        <Grid item xs={12} md={3} mt={4}>
+          <Typography
+            variant="h5"
+            component={"h3"}
+            mb={3}
+            fontWeight={700}
+            color="text.secondary"
+          >
+            Authors
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} md={9} mt={4}>
+          <Typography
+            variant="h5"
+            component={"h3"}
+            mb={3}
+            fontWeight={700}
+            color="text.secondary"
+          >
+            Blogs
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default HomePage;
