a0a0b23371d8ce8a770dcabeaa2c8837cc931db4
Parham Abolghasemi <parhamab17@gmail.com>
2022-07-30 08:54:44 +0430

    render the html inside react (bugs fixed)

--- src/components/authors/AuthorPage.js
+++ src/components/authors/AuthorPage.js
@@ -54,7 +54,7 @@ const AuthorPage = () => {
           </Grid>
 
           <Grid item>
-            {html}
+            <div dangerouslySetInnerHTML={{ __html: html }}></div>
           </Grid>
         </Grid>
       </Container>
